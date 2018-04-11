a9f0bbfb8787af770016af8b42a4a89c1655b6a6
Nikesh Bajaj <bajaj.nikkey@gmail.com>
2018-04-11 07:48:05 +0100

    Update LFSRv2.m

--- LFSRv2.m
+++ LFSRv2.m
@@ -25,15 +25,15 @@ c(1,:)=s;
 m=length(t);
 N = 2^n-2
 for k=1:N;
-b(1)=xor(s(t(1)), s(t(2)));
-if m>2;
-    for i=1:m-2;
-    b(i+1)=xor(s(t(i+2)), b(i));
+    b(1)=xor(s(t(1)), s(t(2)));
+    if m>2;
+        for i=1:m-2;
+        b(i+1)=xor(s(t(i+2)), b(i));
+        end
     end
-end
-j=1:n-1;
-s(n+1-j)=s(n-j);
-s(1)=b(m-1);
-c(k+1,:)=s;
+    j=1:n-1;
+    s(n+1-j)=s(n-j);
+    s(1)=b(m-1);
+    c(k+1,:)=s;
 end
 seq=c(:,n)';
