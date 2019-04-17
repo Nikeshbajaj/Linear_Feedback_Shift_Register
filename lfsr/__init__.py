34d33ba26a3008ade13db8f72b7939857bd6a633
Nikesh Bajaj <bajaj.nikkey@gmail.com>
2019-04-17 19:34:00 +0200

    Update and rename lfsr/__init__.py to pylfsr/__init__.py

--- lfsr/__init__.py
+++
@@ -1,4 +0,0 @@
-name = "Linear Feedback Shift Register"
-__version__ = '0.0.1'
-
-from .lfsr import LFSR
---
+++ pylfsr/__init__.py
@@ -0,0 +1,22 @@
+name = "Linear Feedback Shift Register"
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.YaN   # Alpha release
+#   X.YbN   # Beta release
+#   X.YrcN  # Release Candidate
+#   X.Y     # Final release
+#
+# Dev branch marker is: 'X.Y.devN' where N is an integer.
+#
+
+__version__ = '1.0.1'
+__author__ = 'Nikesh Bajaj'
+
+from .pylfsr import LFSR
