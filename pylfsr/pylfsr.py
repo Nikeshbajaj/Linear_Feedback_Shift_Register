a1fe665a226c00c26439f14fdc913b4d57954e85
Nikesh Bajaj <bajaj.nikkey@gmail.com>
2021-04-29 06:41:02 +0100

    Added A5/1, Geffe Generators and Viz method

--- pylfsr/pylfsr.py
+++ pylfsr/pylfsr.py
@@ -1,11 +1,16 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 '''
 Author @ Nikesh Bajaj
 first created : Date: 22 Oct 2017
-Updated on : 21 Apr 2021
+Updated on : 29 Apr 2021
            : fixed bugs (1) not counting first outbit correctly (2) Exception in info method
 		   : added test properties (1) Balance (2) Runlength (3) Autocorrelation
-Version : 1.0.5
+           : improved functionalities
+           : added Viz function
+           : added A5/1 and Geffe Generator
+Version : 1.0.6
 Contact: n.bajaj@qmul.ac.uk
        : http://nikeshbajaj.in
 '''
@@ -141,7 +146,7 @@ class LFSR():
 	## Example 3 ## TO visualize the process with 3-bit LFSR, with default counter_start_zero = True
 	>>> state = [1,1,1]
 	>>> fpoly = [3,2]
-	>>> L = LFSR(initstate=state,fpoly=fpoly)
+	>>> L = LFSR(initstate=state,fpoly=fpoly,counter_start_zero=True)
 	>>> print('count \t state \t\toutbit \t seq')
 	>>> print('-'*50)
 	>>> for _ in range(15):
@@ -169,7 +174,7 @@ class LFSR():
 	--------------------------------------------------
 	Output:  [1 1 1 0 0 1 0 1 1 1 0 0 1 0 1]
 
-	## Example 4 ## TO visualize the process with 3-bit LFSR, with default counter_start_zero = False
+	## Example 4.1 ## To visualize the process with 3-bit LFSR, with counter_start_zero = False
 	>>> state = [1,1,1]
 	>>> fpoly = [3,2]
 	>>> L = LFSR(initstate=state,fpoly=fpoly,counter_start_zero=False)
@@ -182,23 +187,25 @@ class LFSR():
 	>>> print('Output: ',L.seq)
 	count 	 state 		outbit 	 seq
 	--------------------------------------------------
-	0		[1 1 1]		-1	[-1]
-	1		[0 1 1]		1	[1]
-	2		[0 0 1]		1	[1 1]
-	3		[1 0 0]		1	[1 1 1]
-	4		[0 1 0]		0	[1 1 1 0]
-	5		[1 0 1]		0	[1 1 1 0 0]
-	6		[1 1 0]		1	[1 1 1 0 0 1]
-	7		[1 1 1]		0	[1 1 1 0 0 1 0]
-	8		[0 1 1]		1	[1 1 1 0 0 1 0 1]
-	9		[0 0 1]		1	[1 1 1 0 0 1 0 1 1]
-	10		[1 0 0]		1	[1 1 1 0 0 1 0 1 1 1]
-	11		[0 1 0]		0	[1 1 1 0 0 1 0 1 1 1 0]
-	12		[1 0 1]		0	[1 1 1 0 0 1 0 1 1 1 0 0]
-	13		[1 1 0]		1	[1 1 1 0 0 1 0 1 1 1 0 0 1]
-	14		[1 1 1]		0	[1 1 1 0 0 1 0 1 1 1 0 0 1 0]
+	1	[1 1 1]		1	[1]
+	2	[0 1 1]		1	[1 1]
+	3	[0 0 1]		1	[1 1 1]
+	4	[1 0 0]		0	[1 1 1 0]
+	5	[0 1 0]		0	[1 1 1 0 0]
+	6	[1 0 1]		1	[1 1 1 0 0 1]
+	7	[1 1 0]		0	[1 1 1 0 0 1 0]
+	8	[1 1 1]		1	[1 1 1 0 0 1 0 1]
+	9	[0 1 1]		1	[1 1 1 0 0 1 0 1 1]
+	10	[0 0 1]		1	[1 1 1 0 0 1 0 1 1 1]
+	11	[1 0 0]		0	[1 1 1 0 0 1 0 1 1 1 0]
+	12	[0 1 0]		0	[1 1 1 0 0 1 0 1 1 1 0 0]
+	13	[1 0 1]		1	[1 1 1 0 0 1 0 1 1 1 0 0 1]
+	14	[1 1 0]		0	[1 1 1 0 0 1 0 1 1 1 0 0 1 0]
 	--------------------------------------------------
 	Output:  [1 1 1 0 0 1 0 1 1 1 0 0 1 0 1]
+
+	## Example 4.2 ## To visualize LFSR
+	L.Viz(show=False, show_labels=False,title='R1')
 
 	## Example 5  ## 23 bit LFSR with custum state and feedback polynomial
 
@@ -220,6 +227,9 @@ class LFSR():
        1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
        1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
        1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1])
+
+	>>> L.changeFpoly(newfpoly =[23,21])
+	>>> seq1 = L.runKCycle(20)
 
 	##  Example 6 ## testing the properties
 	>>> state = [1,1,1,1,0]
@@ -429,8 +439,10 @@ class LFSR():
 		-------
 		output bit : binary
 		'''
-		if self.verbose: print('S: ', self.state)
+		if self.verbose:
+			print('S: ', self.state)
 		if self.counter_start_zero:
+			self.outbit = self.state[-1]
 			if self.count ==0:
 				self.seq = np.array([self.state[-1]])
 			else:
@@ -441,12 +453,13 @@ class LFSR():
 			for i in range(2, len(self.fpoly)):
 				b = np.logical_xor(self.state[self.fpoly[i] - 1], b)
 
-		self.outbit = self.state[-1]
+		#self.outbit = self.state[-1]
 		self.state = np.roll(self.state, 1)
 		self.feedbackbit = b * 1
 		self.state[0] = self.feedbackbit
 
 		if not(self.counter_start_zero):
+			self.outbit = self.state[-1]
 			if self.count ==0:
 				self.seq = np.array([self.state[-1]])
 			else:
@@ -807,6 +820,434 @@ class LFSR():
 		    plt.grid(alpha=0.4)
 		    plt.show()
 		return result, (shift,rxx)
+
+	def getSeq(self):
+		return ''.join(self.seq.copy().astype(str))
+	def getState(self):
+	    return ''.join(self.state.copy().astype(str))
+	def arr2str(self,arr):
+		return ''.join(arr.copy().astype(str))
+	def Viz(self,ax=None,show=True,fs=25,show_labels=False,title='',title_loc='left',box_color='lightblue',alpha=0.5):
+		'''
+		ax: axis to plot, if None, new axis will be created, (default None)
+		show: if True, plt.show() will be excecuted, (default True)
+		fs:  fontsize (default 25)
+		show_label: if true, will display names
+		title: str, title of figure, default '',
+		title_loc, alignment of title, 'left', 'right', 'center', (default 'left')
+		'''
+		state = self.state
+		fpoly = self.fpoly
+		seq = self.getSeq()
+		outbit = self.outbit
+		feedbit = self.feedbackbit
+		PlotLFSR(state,fpoly,seq=seq,ob=outbit,fb=feedbit,fs=fs,ax=ax,show_labels=show_labels,title=title,title_loc=title_loc,box_color=box_color,alpha=alpha)
+		if  show: plt.show()
+
+
+
+def drawR(ax,x=0,y=0,s=1,alpha=0.5,color='lightblue'):
+    rect = patches.Rectangle((x-s/2, y-s/2), s, s, linewidth=1, edgecolor='k', facecolor=color,alpha=alpha)
+    ax.add_patch(rect)
+def PlotLFSR(state,fpoly,seq='',ob=None,fb=None,fs=25,ax=None,show_labels=False,title='',title_loc='left',box_color='lightblue',alpha=0.5):
+	'''
+    -----  Plot LFSR ----
+    state: current state of LFSR
+    fpoly:  feedback polynomial of LFSR
+    seq: str, output sequence
+    ob: output bit
+    fb: feedback bit
+    ax: axis to plot, if None, new axis will be created, (default None)
+
+	show: if True, plt.show() will be excecuted, (default True)
+	fs:  fontsize (default 25)
+	show_label: if true, will display names
+	title: str, title of figure, default '',
+	title_loc, alignment of title, 'left', 'right', 'center', (default 'left')
+	box_color: color of register box, default='lightblue'
+	'''
+	M = len(state)
+	ym = 3.5
+	if ax is None:
+	    fig, ax = plt.subplots(figsize=(M+5,ym))
+
+	s=1
+	xs = 3
+	ys = ym-1
+	last_x= xs
+
+	for k in range(M):
+	    x,y = xs+k,ys
+	    ax.text(x,y,str(state[k]),ha='center',va = 'center',fontsize=fs)
+
+	    if k==0:
+	        x1, y1 = [x-1.5*s, x-s/2], [y, y]
+	        ax.plot(x1, y1,marker = '>',color='k',markevery=(1,1),ms=10)
+
+	    if k+1 in fpoly:
+	        x1, y1 = [x, x], [y-s/2, y-1.5*s]
+	        ax.plot(x1, y1,marker = '.',color='k')
+	        ax.plot(x,y-1.5*s,marker = '+',color='b',ms=15,mew=3)
+	        ax.plot(x,y-1.5*s,marker = 'o',color='b',ms=15,mfc='none',mew=2)
+	        if last_x<x: last_x=x
+	    drawR(ax,x=x,y=y,s=s,alpha=alpha,color=box_color)
+
+    #if fb is not None: ax.text(xs-1.7*s,y,'fb = '+str(fb),fontsize=fs-7,va = 'bottom')
+	if fb is not None:
+	    if show_labels:
+	        ax.text(xs-1.7*s,y,'fb = '+str(fb),fontsize=fs-7,va = 'bottom')
+	    else:
+	        ax.text(xs-1.7*s,y,str(fb),fontsize=fs,va = 'bottom',color='b')
+
+	x1, y1 = [last_x,xs-1.5*s ], [y-1.5*s, y-1.5*s]
+	ax.plot(x1, y1,marker = '<',color='k',markevery=(1,1),ms=10)
+
+	x2, y2 = [xs-1.5*s,xs-1.5*s], [y-1.5*s,y]
+	ax.plot(x2, y2,marker = 'd',color='k',markevery=(1,1))
+
+	x1, y1 = [x+s/2, x+1.5*s], [y, y]
+	ax.plot(x1, y1,marker = '>',color='k',markevery=(1,1),ms=10)
+	#if ob is not None: ax.text(x+1.7*s,y,'ob = '+str(ob),fontsize=fs-7,va = 'bottom')
+	if ob is not None:
+	    if show_labels:
+	        ax.text(x+1.7*s,y,'ob = '+str(ob),fontsize=fs-7,va = 'bottom')
+	    else:
+	        ax.text(x+1.7*s,y,str(ob),fontsize=fs,va = 'bottom',color='b')
+
+	if len(seq):
+	    ax.text(0,0,'Output seq = '+seq,fontsize=0.7*fs,color='b',ha='left')
+	ax.axis('off')
+	if title!='': plt.title(title,fontsize=fs,loc=title_loc)
+
+class A5_1():
+	'''
+	A5/1 GSM Stream Cipher
+	----------------------
+	#TODO
+		1.doc
+		2.check the output sequence
+	Ref:  https://en.wikipedia.org/wiki/A5/1
+
+	Example
+	--------
+
+	import numpy as np
+	import matplotlib.pyplot as plt
+	from pylfsr import A5_1
+
+	A5 = A5_1(key='random')
+	print('key: ',A5.key)
+	A5.R1.Viz(title='R1')
+	A5.R2.Viz(title='R2')
+	A5.R3.Viz(title='R3')
+
+	print('key: ',A5.key)
+	print()
+	print('count \t cbit\t\tclk\t R1_R2_R3\toutbit \t seq')
+	print('-'*80)
+	for _ in range(15):
+	    print(A5.count,A5.getCbits(),A5.clock_bit,A5.getLastbits(),A5.outbit,A5.getSeq(),sep='\t')
+	    A5.next()
+	print('-'*80)
+	print('Output: ',A5.seq)
+
+	A5.runKCycle(1000)
+	A5.getSeq()
+
+	'''
+	def __init__(self,key='random',k1='ones',k2='random',k3='ones',counter_start_zero=True):
+
+	    self.M1,self.M2,self.M3 =19,22,23
+	    self.M = self.M1+self.M2+self.M3
+	    self.counter_start_zero = counter_start_zero
+
+
+	    if key is not None:
+	        key = self.key_frmt(self.M,key)
+	        assert len(key)==self.M
+	        #key should be of length = 19+22+23 for three LFSRs
+
+	        self.k1 = key[:self.M1]
+	        self.k2 = key[self.M1:self.M1+self.M2]
+	        self.k3 = key[self.M1+self.M2:]
+
+	        assert len(self.k1)==self.M1 and len(self.k2)==self.M2 and len(self.k3)==self.M3
+	        self.key = ''.join(key.copy().astype(str))
+
+	    else:
+	        self.k1 = self.key_frmt(n=self.M1,ktype=k1)
+	        self.k2 = self.key_frmt(n=self.M2,ktype=k2)
+	        self.k3 = self.key_frmt(n=self.M3,ktype=k3)
+	        assert len(self.k1)==self.M1 and len(self.k2)==self.M2 and len(self.k3)==self.M3
+
+	        self.key = ''.join([''.join(k.copy().astype(str)) for k in [self.k1, self.k2, self.k3]])
+
+
+	    self.R1 = LFSR(initstate=self.k1, fpoly = [19,18,17,14],counter_start_zero=counter_start_zero)
+	    self.R2 = LFSR(initstate=self.k2, fpoly = [22,21],counter_start_zero=counter_start_zero)
+	    self.R3 = LFSR(initstate=self.k3, fpoly = [23,22,21,8],counter_start_zero=counter_start_zero)
+	    self.state = np.r_[self.R1.state, self.R2.state,self.R3.state]
+
+	    # clocking bits
+	    self.c1 = self.R1.state[8]
+	    self.c2 = self.R2.state[10]
+	    self.c3 = self.R3.state[10]
+
+	    self.count = 0
+	    self.seq = np.array([])
+	    self.outbit = -1
+	    self.clock_bit = -1
+
+
+
+	def key_frmt(self,n,ktype):
+		if isinstance(ktype, str):
+		    if ktype == 'ones':
+		        ikey = np.ones(n).astype(int)
+		    elif ktype == 'random':
+		        ikey = np.random.randint(0, 2,n).astype(int)
+		    elif len(ktype)==n:
+		        ikey = np.array([int(b) for b in ktype]).astype(int)
+		    else: raise Exception('Unknown intial state')
+		    return ikey
+		elif isinstance(ktype,list):
+		    return np.array(ktype)
+		elif isinstance(ktype,np.ndarray):
+		    return ktype
+		else:
+		    raise Exception('Unknown key type one of [binary string, list, np.array]')
+
+	def next(self):
+		'''
+		#TODO check the output sequence
+		'''
+		# clocking bits
+
+		if self.count:
+			if self.c1==self.clock_bit: self.R1.next()
+			if self.c2==self.clock_bit: self.R2.next()
+			if self.c3==self.clock_bit: self.R3.next()
+
+		self.state = np.r_[self.R1.state, self.R2.state,self.R3.state]
+		self.outbit = np.logical_xor(np.logical_xor(self.R1.state[-1],self.R2.state[-1]),self.R3.state[-1])*1
+
+		self.seq  = np.append(self.seq, self.outbit).astype(int)
+
+		self.count+=1
+
+		self.c1 = self.R1.state[8]
+		self.c2 = self.R2.state[10]
+		self.c3 = self.R3.state[10]
+		self.clock_bit = (self.c1+self.c2+self.c3 > 1)*1
+		return self.outbit
+
+	def getLastbits(self):
+	    return [self.R1.state[-1],self.R2.state[-1],self.R3.state[-1]]
+	def getCbits(self):
+	    return [self.R1.state[8],self.R2.state[10],self.R3.state[10]]
+	def getSeq(self):
+	    return ''.join(self.seq.copy().astype(str))
+	def getState(self):
+	    return ''.join(self.state.copy().astype(str))
+	def arr2str(self,arr):
+		return ''.join(arr.copy().astype(str))
+	def runKCycle(self, k):
+	    '''
+	    Run k cycles and update all the Parameters
+
+	    Parameters
+	    ----------
+	    k : int
+
+	    Returns
+	    -------
+	    tempseq : shape =(k,), output binary sequence of k cycles
+	    '''
+	    tempseq = [self.next() for i in range(k)]
+	    return np.array(tempseq)
+
+class Geffe():
+	'''
+	Geffe Generator
+	---------------
+	Combining K LFSR in non-linear manner
+	linear complexity
+
+	Parameters
+	----------
+	K+1 LFSRs
+
+	kLFSR_list: list of K LFSR, output of one of these is choosen at any time, depending on cLFSR
+	cLFSR: clocking LFSR
+
+	K should be power of 2. 2,4,8,... 128
+
+	Ref: Schneier, Bruce. Applied cryptography: protocols, algorithms, and source code in C. john wiley & sons, 2007.
+	Chaper 16
+
+
+	Example
+	--------
+
+	import numpy as np
+	import matplotlib.pyplot as plt
+	from pylfsr import Geffe, LFSR
+
+	kLFSR = [LFSR(initstate='random') for _ in range(8)]
+	cLFSR = LFSR(initstate='random')
+
+	GG = Geffe(kLFSR_list=kLFSR, cLFSR=cLFSR)
+
+	print('key: ',GG.getState())
+	print()
+	for _ in range(50):
+	    print(GG.count,GG.m_count,GG.outbit_k,GG.sel_k,GG.outbit,GG.getSeq(),sep='\t')
+	    GG.next()
+
+	GG.runKCycle(1000)
+	GG.getSeq()
+	'''
+	def __init__(self,kLFSR_list,cLFSR):
+
+	    self.K = len(kLFSR_list)
+	    assert isinstance(cLFSR,LFSR)
+	    assert [isinstance(Rk,LFSR) for Rk in kLFSR_list]
+	    assert self.K>1
+	    assert (self.K & (self.K-1) == 0) and self.K != 0
+	    #K (list of LFSR) should be power of 2
+
+	    self.m = np.log2(self.K).astype(int)
+
+	    self.kLFSR_list = kLFSR_list
+	    self.cLFSR = cLFSR
+
+	    self.count=0
+	    self.m_count =0
+	    self.seq =np.array([])
+	    self.outbit = -1
+	    self.sel_k = -1
+	    self.outbit_k = [Rk.state[-1] for Rk in self.kLFSR_list]
+	    self.state = np.hstack([R.state for R in self.kLFSR_list+[self.cLFSR]])
+	    self.state_k = np.hstack([R.state for R in self.kLFSR_list])
+	    self.state_c = self.cLFSR.state
+
+
+	def getSel(self):
+	    sel =  self.cLFSR.runKCycle(self.m)
+	    self.m_count+=self.m
+	    sel = ''.join(sel.astype(str))
+	    return int(sel, 2)
+	def next(self):
+	    if self.count:
+	        _ = [Rk.next() for Rk in self.kLFSR_list]
+
+	    self.outbit_k = [Rk.state[-1] for Rk in self.kLFSR_list]
+	    self.sel_k = self.getSel()
+	    self.outbit = self.outbit_k[self.sel_k]
+
+	    self.seq = np.append(self.seq,self.outbit).astype(int)
+
+	    self.state = np.hstack([R.state for R in self.kLFSR_list+[self.cLFSR]])
+	    self.state_k = np.hstack([R.state for R in self.kLFSR_list])
+	    self.state_c = self.cLFSR.state
+
+	    self.count+=1
+	    return self.outbit
+
+	def getSeq(self):
+	    return ''.join(self.seq.copy().astype(str))
+	def getState(self):
+	    return ''.join(self.state.copy().astype(str))
+	def arr2str(self,arr):
+		return ''.join(arr.copy().astype(str))
+
+	def runKCycle(self, k):
+	    '''
+	    Run k cycles and update all the Parameters
+
+	    Parameters
+	    ----------
+	    k : int
+
+	    Returns
+	    -------
+	    tempseq : shape =(k,), output binary sequence of k cycles
+	    '''
+	    tempseq = [self.next() for i in range(k)]
+	    return np.array(tempseq)
+
+class Geffe3():
+    '''
+    Geffe Generator
+    ---------------
+    Combining three LFSR in non-linear manner
+    linear complexity: If the LFSRs have lengths n1, n2, and n3, respectively, then the linear
+    complexity of the generator is  = (n1 + 1)n2 + n1n3
+
+    output bit at any time is
+
+    b = (r1 ^ r2) • ((¬ r1) ^ r3)
+
+    where r1,r2,r3 are the outbit of three LFSRs respectively
+
+    Ref: Schneier, Bruce. Applied cryptography: protocols, algorithms, and source code in C. john wiley & sons, 2007.
+    Chaper 16
+
+    '''
+    def __init__(self,R1,R2,R3):
+
+        assert isinstance(R1,LFSR)
+        assert isinstance(R2,LFSR)
+        assert isinstance(R3,LFSR)
+
+        self.R1 = R1
+        self.R2 = R2
+        self.R3 = R3
+        self.count=0
+        self.seq =[]
+        self.state = np.r_[self.R1.state, self.R2.state,self.R3.state]
+        self.next()
+
+    def next(self):
+        if self.count:
+            self.R1.next()
+            self.R2.next()
+            self.R3.next()
+        self.r1 = self.R1.state[-1]
+        self.r2 = self.R2.state[-1]
+        self.r3 = self.R3.state[-1]
+
+        b1 = np.logical_and(self.r1,self.r2)
+        b2 = np.logical_and(not(self.r1),self.r2)
+        self.outbit = np.logical_xor(b1,b2)*1
+
+        self.seq = np.append(self.seq,self.outbit).astype(int)
+
+        self.state = np.r_[self.R1.state, self.R2.state,self.R3.state]
+        self.count+=1
+        return self.outbit
+    def getSeq(self):
+        return ''.join(self.seq.copy().astype(str))
+    def getState(self):
+        return ''.join(self.state.copy().astype(str))
+    def arr2str(self,arr):
+    	return ''.join(arr.copy().astype(str))
+
+    def runKCycle(self, k):
+        '''
+        Run k cycles and update all the Parameters
+
+        Parameters
+        ----------
+        k : int
+
+        Returns
+        -------
+        tempseq : shape =(k,), output binary sequence of k cycles
+        '''
+        tempseq = [self.next() for i in range(k)]
+        return np.array(tempseq)
+
 
 if __name__ == '__main__':
 	import doctest
